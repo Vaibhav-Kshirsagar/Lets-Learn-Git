a2fd75e993c650982cefd3ff89d0330f7efcfa87
Vaibhav <maulikshirsagarvk18@gmail.com>
2022-11-28 00:35:25 +0700

    Adding diff.java file

---
+++ sum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sum{
+    public static void main(String argu[]){
+        int a = 5; 
+        int b = 6;
+        int sum = a + b;
+        System.out.print("Sum is "+ sum);    
+    }
+}
