d5f332c3cf2649d93afe484dde05bbcea78da213
Vaibhav <maulikshirsagarvk18@gmail.com>
2022-11-28 15:44:34 +0700

    modified diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class diff {
+    public static void main(String argu[]){
+        int a = 5; 
+        int b = 6;
+        int diff = a + b;
+        System.out.print("diff is "+ diff);    
+    }
+
+}
